6b04bc130de319fe35578f7d4eb57ae4e876db9f
João Eduardo Meneses <jeduado.meneses@gmail.com>
2023-06-14 23:53:50 -0300

    Criação JPA, Repository, DTOs, Controller e tabela no bd de Consultas

--- src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 import med.voll.api.domain.endereco.Endereco;
 
 @Table(name="pacientes")
-@Entity(name="pacientes")
+@Entity(name="Paciente")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
